cd594170615dfaa8ae941ece222340e5d99ff706
Екатерина <alexbir.blackbox@gmail.com>
2024-02-25 11:59:52 +0300

    add style for button, add textarea

--- words-flip/words-flip/src/pages/index.tsx
+++ words-flip/words-flip/src/pages/index.tsx
@@ -1,17 +1,23 @@
-import Image from "next/image";
+
 import { Inter } from "next/font/google";
+import { Button } from '@/components/ui/button'
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`max-w-screen-md flex min-h-screen flex-col p-24 gap-5 ${inter.className}`}
     >
-     <a href="/training" className="relative">
-        <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-400"></span>
-        <span className="fold-500 relative inline-block h-full w-full rounded border-2 border-black bg-black px-3 py-1 text-base text-white transition duration-100 hover:bg-gray-900 hover:text-yellow-500 dark:bg-black">Начать изучать слова</span>
-    </a>
+      <Label htmlFor="message-2">Укажите тематику слов, которые хотите изучать</Label>
+      <Textarea placeholder="Прямо в этом окне" id="message-2" />
+      <p className="text-sm text-muted-foreground">
+        Так мы сможем подобрать вам слова, которые вы хотите изучить
+      </p>
+      <Button>Начать изучать слова</Button>
+
     </main>
   );
 }
